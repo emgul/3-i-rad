25f076e15aa96ceda08b172c5aa3f13f649b7c0e
heem14 <te14.emil.henriksson@umea.nti.se>
2016-09-09 10:25:13 +0200

    inte kommit mycket längre än föra gången

--- src/Tre_i_rad.java
+++ src/Tre_i_rad.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Tre_i_rad {
     public static void main(String[] args) {
-        System.out.println("Start");
+
+        String [][] board = {   {"1","2","3"},
+                                {"4","5","6"},
+                                {"7","8","9"} };
+
+
+        System.out.print(board);
+
+
+
+
     }
 }
