78d5588cf4bed41a1f47d5a975356e3b55d381cf
heem14 <te14.emil.henriksson@umea.nti.se>
2016-09-09 10:54:35 +0200

    Skapade en array för spelplanen

--- src/Tre_i_rad.java
+++ src/Tre_i_rad.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Tre_i_rad {
+
     public static void main(String[] args) {
 
-        String [][] board = {   {"1","2","3"},
-                                {"4","5","6"},
-                                {"7","8","9"} };
+        int[] arr;
+        arr = new int[9];
 
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        arr[5] = 6;
+        arr[6] = 7;
+        arr[7] = 8;
+        arr[8] = 9;
 
-        System.out.print(board);
+        System.out.println(java.util.Arrays.toString(arr));
 
+        Scanner move = new Scanner(System.in);
 
 
 
